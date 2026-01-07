c73e7abc2f8c8cec4a9618d07b87f85413ff0644
Damilola Fakorede <dammy.fakorede@gmail.com>
2026-01-07 15:53:11 +0100

    upload video to s3 and details to dynamodb

--- src/components/ShortThumbnail.tsx
+++ src/components/ShortThumbnail.tsx
@@ -3,7 +3,7 @@ import { IVideo } from "../interfaces/IVideo"
 
 const ShortThumbnail = ({video}: {video: IVideo}) => {
   return (
-   <div className="w-[100%] max-h-[460px] h-[460px] p-2 hover:bg-gray-100 cursor-pointer transition-colors duration-300">
+   <div className="w-full max-h-[460px] h-[460px] p-2 hover:bg-gray-100 cursor-pointer transition-colors duration-300">
       <img src={video.thumbnailImgUrl} alt="" className="h-88 img-cover rounded-xl hover:rounded-none transition-rounded duration-300" />
       <div className="flex justify-between mt-3">
           <div className="flex gap-4">
